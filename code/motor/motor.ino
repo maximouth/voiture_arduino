550646ef755a927b77cd0b5b34f9efec86ad2a45
Moi-meme <maximouth@localhost.localdomain>
2015-05-16 13:01:17 +0200

    code moteur bon

--- code/motor/motor.ino
+++ code/motor/motor.ino
@@ -1,15 +1,38 @@
-const int input1 = 3;
-const int input2 = 4;
+const int input1 = 8;
+const int input2 = 3;
+const int pwm = 11;
 
 int x = 0;
 int sens = 0; // 0 pour incrementer - 1 pour decrementer
 
 void setup() {
-
+  
+  pinMode(input1, OUTPUT);
+  pinMode(input2, OUTPUT);
+  pinMode(pwm, OUTPUT);
 
 }
 
 void loop() {
 
+  analogWrite (pwm,x);
+  if (sens = 0) {
+    x++;
+    if (x > 255) {
+      sens = 1;
+      x = 255;
+    }
+  }
+  else {
+    x--;
+    if (x < 1) {
+      sens = 0;
+      x = 1;
+    }
+    
+  }
+
+  delay (500);
+  
   
 }
